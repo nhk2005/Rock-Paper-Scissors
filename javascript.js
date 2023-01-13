6804cd416fd9c59c00228c2a2d353d7fd331e84f
nhk2005 <nhk2005.personal@gmail.com>
2023-01-13 15:30:14 +0700

    Add function to decide winner of the game

--- javascript.js
+++ javascript.js
@@ -2,3 +2,13 @@ function getComputerChoice() {
   const rnd = Math.floor(3*Math.random()) + 1;
   return (rnd === 1) ? "ROCK" : (rnd == 2) ? "PAPER" : "SCISSORS";
 }
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice == computerChoice) {
+    return 0;
+  } else if (playerChoice == "ROCK" && computerChoice == "SCISSORS" || playerChoice == "SCISSORS" && computerChoice == "PAPER" || playerChoice == "PAPER" && computerChoice == "ROCK") {
+    return 1;
+  } else {
+    return -1;
+  }
+}
