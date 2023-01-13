60c736fc15cf50051276f7a127eea9f5aa251e50
nhk2005 <nhk2005.personal@gmail.com>
2023-01-13 15:42:39 +0700

    Fix syntax error console.log()

--- javascript.js
+++ javascript.js
@@ -29,7 +29,7 @@ function game() {
       console.log("You win! " + playerChoice + " beats " + computerChoice);
     } else {
       ++computerPoints;
-      console("You lose! " + computerChoice + " beats " + playerChoice);
+      console.log("You lose! " + computerChoice + " beats " + playerChoice);
     }
   }
 
